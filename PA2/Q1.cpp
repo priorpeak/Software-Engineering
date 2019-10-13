fae55b36fa270e6ac9b1caa1da11e1c2ca7b1c28
priorpeak <alexanderjprior@gmail.com>
2019-10-13 13:15:33 -0400

    First commit

---
+++ PA2/Q1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+    string command;
+    int num1, num2 = 0;
+
+    cout << "Please enter a command code: ";
+    cin >> command >> endl;
+
+    while (command != "quit") {
+        switch (command) {
+            case 'f' || 'F':
+
+        }
+    }
+}
